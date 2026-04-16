0c70039f9c3d0e974ce230983cbd4d9e5f8fea62
Da-Smurf <m.cheryl.maafi06@gmail.com>
2026-04-16 18:26:23 +0700

    added writing, links, and about.

--- includes/globals/js/common.js
+++ includes/globals/js/common.js
@@ -5,18 +5,39 @@
 */
 document.querySelectorAll("time").forEach(el => {
   if (!el.dateTime) return;
-
   const date = new Date(el.dateTime);
   if (isNaN(date)) return;
+  const format = el.dataset.format || "full";
 
-  const formatted = new Intl.DateTimeFormat(undefined, {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit"
-  }).format(date);
+  let options;
 
-  el.textContent = formatted
+  switch (format) {
+    case "full":
+        options = {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+        };
+        break;
+    case "date":
+        options = {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+        };
+        break;
+  }
+
+//   const formatted = new Intl.DateTimeFormat(undefined, {
+//     day: "2-digit",
+//     month: "short",
+//     year: "numeric",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit"
+//   }).format(date);
+
+  el.textContent = new Intl.DateTimeFormat(undefined, options).format(date);
 });
